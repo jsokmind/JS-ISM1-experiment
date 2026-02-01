5124ff0ef0c754799ae1cbe56f97fb7b357282f8
jsokmind <j.sokmind@gmail.com>
2026-02-01 14:19:57 -0600

    Fix formatting and improve instructions in app.py

--- app.py
+++ app.py
@@ -687,10 +687,10 @@ if not st.session_state.started:
     "To be eligible for the free but random **$50 participation prize**, you must **complete all rounds of the game** "
     "and **submit the short form** that will appear at the end of the experiment.\n\n"
     "Lastly, please make sure that you are in a **DISTRACTION-FREE, QUIET environment. Ensure that you can provide this experiment your full and undivided attention.** Please proceed ONLY if you are in this said, optimal environment. Thank you. \n\n"
-    "    "
     
     "Good luck!" 
     )
+    "    "
     st.write(f"Starting balance: {st.session_state.balance}")
     st.button("Start Experiment", on_click=_start_experiment)
     
